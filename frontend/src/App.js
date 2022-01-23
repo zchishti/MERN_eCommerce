669c51cd43d4036164af9b60c99fc30440dd5181
UCO\zchishti <zchishti91@gmail.com>
2022-01-22 22:10:14 -0600

    Header and Footer components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Header />
-        <main>
+        <main className='py-3'>
           <Container>
             <h1>Welcome to Online Store</h1>
           </Container>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,16 +4,17 @@ import {Container, Nav, Navbar} from 'react-bootstrap';
 const Header = () => {
     return (
         <header>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    </Nav>
-                   
-                </Navbar.Collapse>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">Online Store</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
