eba45f752f5828daccc7f9b464039730f035388f
UCO\zchishti <zchishti91@gmail.com>
2022-02-07 23:22:00 -0600

    Controllers added for user auth route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,9 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js'
+import { getProducts, getProductById } from '../controllers/productControllers.js';
 
-// @desc    fetch all products
-// route    Get api/products
-// access   Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}));
 
-// @desc    fetch single product
-// route    Get api/products/:id
-// access   Public
-router.get('/:id', asyncHandler( async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if(product){
-        res.json(product);
-    }else{
-        res.status(404)
-        throw new Error('Product not found');
-    }
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
