43ea6e4ffd07b296e05a52fb5186dd244fe246e8
UCO\zchishti <zchishti91@gmail.com>
2022-02-08 00:21:00 -0600

    Auth middleware to protect routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user and get token
 // route    Get api/users/login
@@ -14,7 +15,7 @@ const authUser = asyncHandler(async (req,res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         });
     }else{
         res.status(401);
@@ -22,6 +23,26 @@ const authUser = asyncHandler(async (req,res) => {
     }
 });
 
+// @desc    Get user profile
+// route    Get api/users/profile
+// access   Private
+const getUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        res.json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin 
+        })
+    }else{
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
+
 export {
-    authUser
+    authUser,
+    getUserProfile
 }
