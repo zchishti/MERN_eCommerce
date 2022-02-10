d4bc29bfd0bf9fa596ea2443efd480ff65af37d2
UCO\zchishti <zchishti91@gmail.com>
2022-02-09 23:22:29 -0600

    Logout and navbar updated

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
 
 
 export const login = (email, password) => async (dispatch) => {
@@ -29,4 +29,9 @@ export const login = (email, password) => async (dispatch) => {
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo');
+    dispatch({ type: USER_LOGOUT });
 }
