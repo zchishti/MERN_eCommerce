f32d25af4ea6235a7028093486a55293ed41a5d1
UCO\zchishti <zchishti91@gmail.com>
2022-02-09 21:08:05 -0600

    Added user registration controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,39 @@ const authUser = asyncHandler(async (req,res) => {
     }
 });
 
+// @desc    Register a user
+// route    Post api/users
+// access   Public
+const registerUser = asyncHandler(async (req,res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+
+    if(userExists){
+        res.status(400);
+        throw new Error(`User already exist with email ${email}`);
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user){
+        res.status(201).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        });
+    }else{
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
+});
+
 // @desc    Get user profile
 // route    Get api/users/profile
 // access   Private
@@ -44,5 +77,6 @@ const getUserProfile = asyncHandler(async (req,res) => {
 
 export {
     authUser,
+    registerUser,
     getUserProfile
 }
